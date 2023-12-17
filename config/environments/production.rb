c956df9ac546d06ce0b90b18d0e45b71dca4ef96
Muhammad Awais <muhammdawaisbinmajid14@gmail.com>
2023-12-17 15:41:38 +0500

    Set the host for production server.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,6 +82,9 @@ Rails.application.configure do
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
 
+  config.action_mailer.default_url_options = { host: 'admin.medicolegalreports.org' }
+  config.action_mailer.default_options = { from: 'kmoholkar@gmail.com' }
+
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
