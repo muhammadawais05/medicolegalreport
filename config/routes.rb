d2975aa8d021653ea5f59fac70c9b79275c42ab2
Muhammad Awais <muhammdawaisbinmajid14@gmail.com>
2024-03-17 17:15:29 +0500

    Uncomment the routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  # devise_for :admin_users, ActiveAdmin::Devise.config
-  # ActiveAdmin.routes(self)
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # get 'homes/index'
-  # root "homes#index"
-  # resources :patient_information, only: %i[edit create show]
-  # post 'patient_information/generate_pdf', to: 'patient_information#generate_pdf', as: 'generate_pdf'
+  get 'homes/index'
+  root "homes#index"
+  resources :patient_information, only: %i[edit create show]
+  post 'patient_information/generate_pdf', to: 'patient_information#generate_pdf', as: 'generate_pdf'
 end
